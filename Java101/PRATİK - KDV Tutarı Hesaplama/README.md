0f8f2683411bfe5d6d855a35a00b8c8a52cf3239
onurerdemirrr <onurerdemiroglu@gmail.com>
2021-10-23 15:59:42 +0300

    PRATIK - KDV Tutarı Hesaplama

--- Java101/PRATİK - KDV Tutarı Hesaplama/README.md
+++ Java101/PRATİK - KDV Tutarı Hesaplama/README.md
@@ -1,6 +1,6 @@
 # PRATİK - KDV Tutarı Hesaplama
 
-> Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayan program.
+> Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan program.
 
 
-![out](https://user-images.githubusercontent.com/35347777/138556125-afd2c106-9ec1-4dca-a7d1-c8fa2ccec79b.gif)
+![out](https://user-images.githubusercontent.com/35347777/138557347-9edffb51-9fb6-4581-b19b-6b0c460b3bef.gif)
--- Java101/PRATİK - KDV Tutarı Hesaplama/src/Main.java
+++ Java101/PRATİK - KDV Tutarı Hesaplama/src/Main.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double Tutar,kdvOran = 0.18 ,kdvliTutar;
+        double Tutar,kdvOran,kdvliTutar;
 
         Scanner Input = new Scanner(System.in);
         System.out.print("Ücret Tutarınızı Giriniz : ");
-
         Tutar = Input.nextDouble();
+        if(Tutar>=0 && Tutar<=1000)
+        {
+            kdvOran = 0.18;
+        }
+        else if(Tutar>1000)
+        {
+            kdvOran = 0.08;
+        }
+        else
+        {
+            kdvOran=0;
+        }
         kdvliTutar = Tutar+Tutar*kdvOran;
 
         System.out.println("KDV'siz Tutar : " + Tutar);
